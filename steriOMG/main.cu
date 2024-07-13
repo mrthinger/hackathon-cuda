89183da1e5a0e2fbbe93510e3eefb77c293715bd
Evan Rusmisel <rusmisel@photonvault.com>
2024-07-13 16:35:22 -0700

    todo

---
+++ steriOMG/main.cu
@@ -0,0 +1,8 @@
+__global__ void map_frame() {}
+
+int main() {
+    // TODO: load video & copy to GPU
+    // TODO: NVDEC frames
+    // TODO: launch map_frame on pixels
+    // TODO: __device__ int reindex()
+}
