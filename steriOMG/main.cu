0498377ba1c2b41715128e4a93f550baa7c7ae47
Evan Rusmisel <rusmisel@photonvault.com>
2024-07-13 17:27:23 -0700

    start kernel

--- steriOMG/main.cu
+++ steriOMG/main.cu
@@ -1,6 +1,10 @@
-__global__ void map_tile(uchar4* d_inframe, uchar4* d_outframe, int w, int h) {
-    int outIdx = threadIdx.y * w + threadIdx.x;
-    d_outframe[outIdx] = d_inframe[outIdx]; // FIXME: offset
+__global__ void map_tile(uchar4* d_inframe, uchar4* d_outframe, int w, int h, int xoffset, int yoffset) {
+    if (threadIdx.y + yoffset > h || threadIdx.x + xoffset > w) return; // tile bounds
+    int yOutCoord = threadIdx.y + yoffset;
+    int xOutCoord = threadIdx.x + xoffset;
+    int outIdx = (threadIdx.y + yoffset) * w + (threadIdx.x + xoffset); // FIXME: skip right half of frame
+    int inIdx = outIdx; // TODO: horizontal offset
+    d_outframe[outIdx] = d_inframe[inIdx];
 }
 
 int main() {
