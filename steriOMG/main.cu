e3e0dcf4016ba055ced76ea6d435363353ae2141
Evan Rusmisel <rusmisel@photonvault.com>
2024-07-13 17:15:32 -0700

    noop framekernel

--- steriOMG/main.cu
+++ steriOMG/main.cu
@@ -1,8 +1,10 @@
-__global__ void map_frame() {}
+__global__ void map_tile(uchar4* d_inframe, uchar4* d_outframe, int w, int h) {
+    int outIdx = threadIdx.y * w + threadIdx.x;
+    d_outframe[outIdx] = d_inframe[outIdx]; // FIXME: offset
+}
 
 int main() {
     // TODO: load video & copy to GPU
     // TODO: NVDEC frames
-    // TODO: launch map_frame on pixels
-    // TODO: __device__ int reindex()
+    // TODO: for tiles: map_tile
 }
